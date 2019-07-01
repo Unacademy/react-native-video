a83f8c3bc1bb166679a51d16714c74f39796b92c
alok <alok@unacademy.com>
2019-07-01 16:27:39 +0530

    added alpha view

--- android/src/main/java/com/brentvatne/react/ReactVideoViewManager.java
+++ android/src/main/java/com/brentvatne/react/ReactVideoViewManager.java
@@ -1,18 +1,15 @@
 package com.brentvatne.react;
 
 import com.brentvatne.react.ReactVideoView.Events;
-import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.common.MapBuilder;
-import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.events.RCTEventEmitter;
-import com.yqritc.scalablevideoview.ScalableType;
+import com.facebook.react.uimanager.annotations.ReactProp;
+
+import java.util.Map;
 
 import javax.annotation.Nullable;
-import java.util.Map;
 
 public class ReactVideoViewManager extends SimpleViewManager<ReactVideoView> {
 
