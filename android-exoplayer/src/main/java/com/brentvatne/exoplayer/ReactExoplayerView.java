b27f997ccd8438a4100675f1ebfbcd889c63e13a
Sai Krishna <m.s.krishna310@gmail.com>
2020-11-03 18:23:36 +0530

    Fixed final variables

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactExoplayerView.java
@@ -226,7 +226,7 @@ class ReactExoplayerView extends FrameLayout implements
   }
 
   @Override
-  public void onKeysGenerated(SecretKeySpec key, IvParameterSpec ivParameterSpec) {
+  public void onKeysGenerated(final SecretKeySpec key, final IvParameterSpec ivParameterSpec) {
        mainHandler.post(new Runnable() {
         @Override
         public void run() {
