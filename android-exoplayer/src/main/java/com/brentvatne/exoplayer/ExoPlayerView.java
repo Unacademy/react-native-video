9ebca6281ea46886b8a8b50e32769699fb7fc63c
AdityaPrakash04 <100825110+AdityaPrakash04@users.noreply.github.com>
2023-06-28 17:59:27 +0530

    hlsmedia segment fix

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ExoPlayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ExoPlayerView.java
@@ -262,7 +262,7 @@ public final class ExoPlayerView extends FrameLayout {
     public void sendFileChangeEventForTime(long time) {
         Object manifest = player.getCurrentManifest();
         if (manifest instanceof HlsManifest) {
-            HlsMediaPlaylist.Segment segment = new HlsMediaPlaylist.Segment("", null,"", 0, 0, time*1000,null, "", "", 0, 0, false);
+            HlsMediaPlaylist.Segment segment = new HlsMediaPlaylist.Segment("", null,"", 0, 0, time*1000,null, "", "", 0, 0, false, ImmutableList.of());
 
             int index = Collections.binarySearch(((HlsManifest) manifest).mediaPlaylist.segments, segment, new Comparator<HlsMediaPlaylist.Segment>() {
                 @Override
