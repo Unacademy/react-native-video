2337ac2cadb9e61c2db75ab6c5c6387501597893
sahil-kamra <95094852+sahil-kamra@users.noreply.github.com>
2025-10-23 18:23:37 +0530

    adding missing import

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ExoPlayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ExoPlayerView.java
@@ -12,7 +12,7 @@ import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-
+import com.brentvatne.react.GLTextureView;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.PlaybackException;
